fe666f1eacc27423d68553f24ebe68a9d9007772
Ribhav Kapur <ribhav.kapur99@gmail.com>
2025-01-24 17:43:18 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -2,6 +2,16 @@ from utils import get_som_labeled_img, check_ocr_box, get_caption_model_processo
 import torch
 from ultralytics import YOLO
 from PIL import Image
+import pandas as pd
+import importlib
+import utils
+importlib.reload(utils)
+import importlib
+import utils
+importlib.reload(utils)
+import time
+
+
 device = "cuda" if torch.cuda.is_available() else "cpu"
 model_path='weights/icon_detect_v1_5/model_v1_5.pt'
 
@@ -10,25 +20,11 @@ som_model = get_yolo_model(model_path)
 som_model.to(device)
 print('model to {}'.format(device))
 
-
-
 # two choices for caption model: fine-tuned blip2 or florence2
-import importlib
-import utils
-importlib.reload(utils)
-from utils import get_som_labeled_img, check_ocr_box, get_caption_model_processor, get_yolo_model
 # caption_model_processor = get_caption_model_processor(model_name="blip2", model_name_or_path="weights/icon_caption_blip2", device=device)
 caption_model_processor = get_caption_model_processor(model_name="florence2", model_name_or_path="weights/icon_caption_florence", device=device)
 
-
-
-
-
 # reload utils
-import importlib
-import utils
-importlib.reload(utils)
-from utils import get_som_labeled_img, check_ocr_box, get_caption_model_processor, get_yolo_model
 
 image_path = 'imgs/google_page.png'
 image_path = 'imgs/windows_home.png'
@@ -51,7 +47,6 @@ draw_bbox_config = {
 }
 BOX_TRESHOLD = 0.05
 
-import time
 print("Starting ocr pipeline")
 start = time.time()
 ocr_bbox_rslt, is_goal_filtered = check_ocr_box(image_path, display_img = False, output_bb_format='xyxy', goal_filtering=None, easyocr_args={'paragraph': False, 'text_threshold':0.8}, use_paddleocr=True)
@@ -63,3 +58,15 @@ print("Starting labeling pipeline")
 cur_time_caption = time.time() 
 dino_labled_img, label_coordinates, parsed_content_list = get_som_labeled_img(image_path, som_model, BOX_TRESHOLD = BOX_TRESHOLD, output_coord_in_ratio=True, ocr_bbox=ocr_bbox,draw_bbox_config=draw_bbox_config, caption_model_processor=caption_model_processor, ocr_text=text,use_local_semantics=True, iou_threshold=0.7, scale_img=False, batch_size=128)
 print(f"labelling time: {cur_time_caption - cur_time_ocr}")
+
+df = pd.DataFrame(parsed_content_list)
+df['ID'] = range(len(df))
+
+print(df)
+
+print(type(text), type(ocr_bbox), type(dino_labled_img), type(label_coordinates), type(parsed_content_list))
+print(text)
+print(ocr_bbox)
+print(dino_labled_img)
+print(label_coordinates)
+print(parsed_content_list)
